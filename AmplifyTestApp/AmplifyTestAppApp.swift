430a70d97d7860f0d788ca55429db54c64fb1c97
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2022-12-06 15:03:42 -0600

    Initial Commit

---
+++ AmplifyTestApp/AmplifyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifyTestAppApp.swift
+//  AmplifyTestApp
+//
+//  Created by Andrew Fairchild on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AmplifyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmplifyTestAppUITests/AmplifyTestAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AmplifyTestAppUITestsLaunchTests.swift
+//  AmplifyTestAppUITests
+//
+//  Created by Andrew Fairchild on 12/6/22.
+//
+
+import XCTest
+
+final class AmplifyTestAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
