5b5f04432c8a57e7b8e194bbfc3d552e460ce55a
Andrew Fairchild <andrew.d.fairchild@gmail.com>
2022-12-07 09:46:14 -0600

    test

--- AmplifyTestApp/AmplifyTestAppApp.swift
+++ AmplifyTestApp/AmplifyTestAppApp.swift
@@ -6,9 +6,32 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSAPIPlugin
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        do {
+            
+            Amplify.Logging.logLevel = .verbose
+            //Configure Amplify as Usual
+            
+            try Amplify.configure()
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
+        return true
+    }
+}
+
 
 @main
 struct AmplifyTestAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ AmplifyTestApp/TestView.swift
@@ -0,0 +1,23 @@
+//
+//  TestView.swift
+//  AmplifyTestApp
+//
+//  Created by Andrew Fairchild on 12/6/22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+/*
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
+*/
